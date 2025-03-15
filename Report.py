ace65d9ef016ea0867d6cd89c460d3b10634d5f4
Nishad Mansoor <38162285+nishadmansoor@users.noreply.github.com>
2025-03-15 00:51:47 -0400

    Add files via upload

---
+++ Report.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown('<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNTA1NTY5ZjMtOWRlZS00Mjc1LWJjNTMtOTQ4NTY3YjM1YTA1IiwidCI6ImE2MTdlYzYwLTBhYjMtNDBiZS05MjhmLWJmMzY1MzA4NDkxYSIsImMiOjF9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+
+st.markdown('<iframe title="clv_powerbi" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZGFmNWMxODAtNDllZS00OTg4LTk2ZmItZjNhNjIyZjA1Y2U2IiwidCI6ImE2MTdlYzYwLTBhYjMtNDBiZS05MjhmLWJmMzY1MzA4NDkxYSIsImMiOjF9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+
+st.markdown('<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYTBhNjJhNjItM2Q0NC00ZjlhLWJkMjQtODlhMDVjZWRkZGQ5IiwidCI6ImE2MTdlYzYwLTBhYjMtNDBiZS05MjhmLWJmMzY1MzA4NDkxYSIsImMiOjF9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+    
+st.markdown('<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNDc1ZDU2ZmMtYWJjMC00YTY3LWI1NzgtMzhiNzg4NjViNjM3IiwidCI6ImVkYmViM2JlLWQ1YTMtNDA5NS1hMjI3LTU2Yzg4ODEzZTZmNyJ9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
